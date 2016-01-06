70cc1fc5a83f53450a9df1d523912e4ed510b485
Francesco Bedussi <francesco@francescobedussi.it>
2016-01-06 18:15:55 +0100

    fix&improvement

--- assets/js/dependencies/app.js
+++ assets/js/dependencies/app.js
@@ -3,65 +3,64 @@
 var scrambleApp = angular.module('scrambleApp', ['ngFileUpload', 'ngImgCrop']);
 
 scrambleApp.controller('ScrambleCtrl', ['$scope', 'Upload', '$rootScope', '$timeout', 'ScrambleService',
-                                        function($scope, Upload, $rootScope, $timeout, ScrambleService) {
-  var randomAvatarBaseUrl = 'http://api.adorable.io/avatars/';
+    function($scope, Upload, $rootScope, $timeout, ScrambleService) {
+        var randomAvatarBaseUrl = 'http://api.adorable.io/avatars/';
   
-  $scope.formDataNewTeam = {};
-  $scope.formDataEditTeam = {};
-  $scope.formDataMember = {};
-  $scope.teams = [];
-  $scope.members = [];
-  $scope.selectedTeam = {};
-  $scope.dataSeved = false;
-  $scope.avatarUrl = '';
-  $scope.membersView = 'gui';
-  $scope.membersWidth = 100/($scope.members.length/2) + '%';
-  $scope.useAllSides = false;
-  $scope.randomAvatarDisplay = false;
-  $scope.randomAvatarUrl = randomAvatarBaseUrl;
-  $scope.croppedDataUrl = null;
-  $scope.avatar = null;
+        $scope.formDataNewTeam = {};
+        $scope.formDataEditTeam = {};
+        $scope.formDataMember = {};
+        $scope.teams = [];
+        $scope.members = [];
+        $scope.selectedTeam = {};
+        $scope.dataSeved = false;
+        $scope.avatarUrl = '';
+        $scope.membersView = 'gui';
+        $scope.membersWidth = 100/($scope.members.length/2) + '%';
+        $scope.useAllSides = false;
+        $scope.randomAvatarDisplay = false;
+        $scope.randomAvatarUrl = randomAvatarBaseUrl;
+        $scope.croppedDataUrl = null;
+        $scope.avatar = null;
+        
+        function selectIfIsTheOnlyTeam() {
+            if ($scope.teams.length === 1) {
+                $scope.selectTeam($scope.teams[0]);
+            }
+        }
   
-  function selectIfIsTheOnlyTeam() {
-    if ($scope.teams.length === 1) {
-        $scope.selectTeam($scope.teams[0]);
-    }
-  }
-  
-  //$locationProvider.html5Mode(true);
-  //$location.path('/en');
-  
-  function updateMembersPos() {
-    $scope.members.forEach(function(member, i) {
-        member.style = getStyle(member.pos, $scope.members.length, {});
-    });
-  }
-  
-  $scope.updateMembersPos = updateMembersPos;
-  
-  ScrambleService.getTeams().then(function(response) {
-    $scope.teams = response;
+    //$locationProvider.html5Mode(true);
+    //$location.path('/en');
     
-    selectIfIsTheOnlyTeam();
-  });
-
-  $scope.addTeam = function() {
-    ScrambleService.addTeam($scope.formDataNewTeam).then(function(response) {
-      $scope.teams.push(response);
-      $scope.formDataNewTeam = {};
-      
-      selectIfIsTheOnlyTeam();
+    function updateMembersPos() {
+        $scope.members.forEach(function(member, i) {
+            member.style = getStyle(member.pos, $scope.members.length, {});
+        });
+    }
+  
+    $scope.updateMembersPos = updateMembersPos;
+  
+    ScrambleService.getTeams().then(function(response) {
+        $scope.teams = response;
+        selectIfIsTheOnlyTeam();
     });
-  };
+
+    $scope.addTeam = function() {
+        ScrambleService.addTeam($scope.formDataNewTeam).then(function(response) {
+            $scope.teams.push(response);
+            $scope.formDataNewTeam = {};
+            
+            selectIfIsTheOnlyTeam();
+        });
+    };
 
     $scope.removeTeam = function(team) {
-      ScrambleService.removeTeam(team).then(function(response) {
-        $scope.teams.splice($scope.teams.indexOf(team), 1);
-        
-        if ($scope.selectedTeam === team) {
-          $scope.selectedTeam = {};
-        }
-      });
+        ScrambleService.removeTeam(team).then(function(response) {
+            $scope.teams.splice($scope.teams.indexOf(team), 1);
+            
+            if ($scope.selectedTeam === team) {
+                $scope.selectedTeam = {};
+            }
+        });
     };
   
     $scope.selectTeam = function(team) {
@@ -131,25 +130,47 @@ scrambleApp.controller('ScrambleCtrl', ['$scope', 'Upload', '$rootScope', '$time
         });   
     };
   
-   $scope.removeMember = function(member) {
-    ScrambleService.deleteAvatar(member).then(function(response) {
-        console.log('avatar deleted' + response);
-        
-        ScrambleService.removeMember(member).then(function(response) {
-            $scope.members.splice($scope.members.indexOf(member), 1);
+    $scope.removeMember = function(member) {
+        ScrambleService.deleteAvatar(member).then(function(response) {
+            console.log('avatar deleted' + response);
             
-            updateMembersPos();
-          });
-        
-      }).catch(function(reason) {
-        console.log('avatar delete error: ' + reason);
-      });
-    
-    
-    
-  };
+            ScrambleService.removeMember(member).then(function(response) {
+                $scope.members.splice($scope.members.indexOf(member), 1);
+                
+                updateMembersPos();
+            });
+            
+        }).catch(function(reason) {
+            console.log('avatar delete error: ' + reason);
+        });
+    };
   
-  function getStyle(pos, totalPos) {
+    //function getSeats(members, customOptions) {
+    //    var options = {
+    //        seats: {
+    //            left: 1,
+    //            top: 5,
+    //            right: 1,
+    //            bottom: 5
+    //        }
+    //    };
+    //    
+    //    $.extend(options, customOptions);
+    //    
+    //    var availableSeats = options.seats;
+    //    var availableMembers = members;
+    //    
+    //    function getSmallestSide(seats) {
+    //        var sortedSeats = Object.keys(seats).sort(function(a,b){return seats[a]-seats[b];});
+    //        return sortedSeats[0];
+    //    }
+    //    
+    //    function seatMember(availableSeats, availableMembers) {
+    //        //code
+    //    }
+    //}
+    
+    function getStyle(pos, totalPos) {
         var options = {
             useAllSides: $scope.useAllSides,
             height: '33'
@@ -193,77 +214,77 @@ scrambleApp.controller('ScrambleCtrl', ['$scope', 'Upload', '$rootScope', '$time
         return style;
     }
     
-  $scope.scrambleMembers = function() {
+    $scope.scrambleMembers = function() {
     var positions = [];
     var j, x, i;
     
     for (i = 1; i<= $scope.members.length; positions.push(i++));
-    for(i = positions.length; i; j = parseInt(Math.random() * i), x = positions[--i], positions[i] = positions[j], positions[j] = x);
-    $scope.members.forEach(function(member, i) {
-        member.pos = positions[i];
-        member.style = getStyle(member.pos, $scope.members.length, {});
-    });
-    
-    ScrambleService.updateMembers($scope.members).then(function(response) {
-       $scope.dataSaved = true;
-       $timeout(function(){
-            $scope.dataSaved = false;
-        }, 2000);
-       //console.log('saved');
-    }, function(response) {
-        $scope.dataSaved = false;
-        //console.log('not saved');
-    });
-  };
-  
-  $scope.editMember = function(member, dataUrl) {
-    
-    function saveChanges(member) {
-        ScrambleService.updateMembers(member).then(function(response) {
-            console.log('member saved');
+        for(i = positions.length; i; j = parseInt(Math.random() * i), x = positions[--i], positions[i] = positions[j], positions[j] = x);
+        
+        $scope.members.forEach(function(member, i) {
+            member.pos = positions[i];
+            member.style = getStyle(member.pos, $scope.members.length, {});
         });
-        member.memberAvatarEdit = false;
-        $scope.croppedDataUrl = null;
+        
+        ScrambleService.updateMembers($scope.members).then(function(response) {
+            $scope.dataSaved = true;
+            $timeout(function(){
+                $scope.dataSaved = false;
+            }, 2000);
+           //console.log('saved');
+        }, function(response) {
+            $scope.dataSaved = false;
+            //console.log('not saved');
+        });
+    };
+  
+    $scope.editMember = function(member, dataUrl) {
+    
+        function saveChanges(member) {
+            ScrambleService.updateMembers(member).then(function(response) {
+                console.log('member saved');
+            });
+            member.memberAvatarEdit = false;
+            $scope.croppedDataUrl = null;
+            $scope.randomAvatarDisplay = false;
+        }
+        
+        if (!dataUrl) {
+            saveChanges(member);
+            return;
+        }
+    
+        ScrambleService.deleteAvatar(member).then(function(response){
+            $scope.uploadAvatar(dataUrl).then(function(resp) {
+                //Uploaded successfully
+                //console.log('upload ok');
+                member.avatarUrl = '/images/' + resp.data.fileName;
+                saveChanges(member);
+                
+            }, function(resp) {
+                // handle error
+                saveChanges(member);
+            }, function(evt) {
+                // progress notify
+                //console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.data.file.name);
+            });   
+        }).catch(function(reason) {
+            console.log('avatar delete error: ' + reason);
+        });
+    
+    };
+  
+    $scope.getRandomAvatar = function(memberName) {
+        $scope.randomAvatarUrl += encodeURIComponent(memberName);
+        $scope.randomAvatarDisplay = true;
+    };
+  
+    $scope.resetNewMember = function() {
+        $scope.formDataMember.name = '';
+        $scope.randomAvatarUrl = randomAvatarBaseUrl;
         $scope.randomAvatarDisplay = false;
-    }
-    
-    if (!dataUrl) {
-        saveChanges(member);
-        return;
-    }
-    
-    ScrambleService.deleteAvatar(member).then(function(response){
-        $scope.uploadAvatar(dataUrl).then(function(resp) {
-            //Uploaded successfully
-            //console.log('upload ok');
-            member.avatarUrl = '/images/' + resp.data.fileName;
-            saveChanges(member);
-            
-        }, function(resp) {
-            // handle error
-            saveChanges(member);
-        }, function(evt) {
-            // progress notify
-            //console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.data.file.name);
-        });   
-    }).catch(function(reason) {
-        console.log('avatar delete error: ' + reason);
-    });
-    
-    
-  };
-  
-  $scope.getRandomAvatar = function(memberName) {
-    $scope.randomAvatarUrl += encodeURIComponent(memberName);
-    $scope.randomAvatarDisplay = true;
-  };
-  
-  $scope.resetNewMember = function() {
-    $scope.formDataMember.name = '';
-    $scope.randomAvatarUrl = randomAvatarBaseUrl;
-    $scope.randomAvatarDisplay = false;
-    $scope.croppedDataUrl = null;
-  };
+        $scope.croppedDataUrl = null;
+    };
 
   
 }]);
