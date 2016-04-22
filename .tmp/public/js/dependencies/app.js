c5e3925dae39808f0d89387a64bf73589b33b797
Francesco Bedussi <francesco@francescobedussi.it>
2016-04-22 09:43:16 +0200

    move service to app.js

--- .tmp/public/js/dependencies/app.js
+++ .tmp/public/js/dependencies/app.js
@@ -1,6 +1,103 @@
 'use strict';
 
-var scrambleApp = angular.module('scrambleApp', ['ngFileUpload', 'ngImgCrop']);
+var scrambleApp = angular.module('scrambleApp', ['ngFileUpload', 'ngImgCrop'])
+.service('ScrambleService', function($http, $q) {
+  return {
+        'getTeams': function() {
+          var defer = $q.defer();
+          $http.get('/api/v1/team').success(function(resp){
+            defer.resolve(resp);
+          }).error( function(err) {
+            defer.reject(err);
+          });
+          return defer.promise;
+        },
+        'getTeamMembers': function(team) {
+          var defer = $q.defer();
+          $http.get('/api/v1/team/' + team.id + '/members/').success(function(resp){
+            defer.resolve(resp);
+          }).error( function(err) {
+            defer.reject(err);
+          });
+          return defer.promise;
+        },
+        'addTeam': function(team) {
+          var defer = $q.defer();
+          $http.post('/api/v1/team', team).success(function(resp){
+            defer.resolve(resp);
+          }).error( function(err) {
+            defer.reject(err);
+          });
+          return defer.promise;
+        },
+        'removeTeam': function(team) {
+          var defer = $q.defer();
+          $http.delete('/api/v1/team/' + team.id).success(function(resp){
+            defer.resolve(resp);
+          }).error( function(err) {
+            defer.reject(err);
+          });
+          return defer.promise;
+        },
+        'editTeam': function(team) {
+          var defer = $q.defer();
+          $http.put('/api/v1/team/' + team.id, team).success(function(resp){
+            defer.resolve(resp);
+          }).error( function(err) {
+            defer.reject(err);
+          });
+          return defer.promise;
+        },
+        'addMember': function(member) {
+          var defer = $q.defer();
+          $http.post('/api/v1/members', member).success(function(resp){
+            defer.resolve(resp);
+          }).error( function(err) {
+            defer.reject(err);
+          });
+          return defer.promise;
+        },
+        'removeMember': function(member) {
+          var defer = $q.defer();
+          $http({
+            method: 'DELETE',
+            url: '/api/v1/members/' + member.id
+          }).success(function(resp){
+            defer.resolve(resp);
+          }).error( function(err) {
+            defer.reject(err);
+          });
+          return defer.promise;
+        },
+        'updateMembers': function(members) {
+          var defer = $q.defer();
+          $http.put('/api/v1/members/', members).success(function(resp){
+            defer.resolve(resp);
+          }).error( function(err) {
+            defer.reject(err);
+          });
+          return defer.promise;
+        },
+        'uploadAvatar': function(fileName, memberName) {
+                var defer = $q.defer();
+                $http.post('/api/v1/members/avatar', {file: fileName, name: memberName}).success(function(resp){
+                defer.resolve(resp);
+            }).error( function(err) {
+                defer.reject(err);
+            });
+            return defer.promise;
+        },
+        'deleteAvatar': function(member) {
+                var defer = $q.defer();
+                $http.delete('/api/v1/members/' + member.id + '/avatar').success(function(resp){
+                defer.resolve(resp);
+            }).error( function(err) {
+                defer.reject(err);
+            });
+            return defer.promise;
+        }
+    };
+});
 
 scrambleApp.controller('ScrambleCtrl', ['$scope', 'Upload', '$rootScope', '$timeout', 'ScrambleService',
     function($scope, Upload, $rootScope, $timeout, ScrambleService) {
--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -42,7 +42,6 @@ var jsFilesToInject = [
   
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
-  'js/service/**/*.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
