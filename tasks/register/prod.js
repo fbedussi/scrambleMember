a0afe09d625a7756696281ed39165c4b13350d98
fbedussi <francesco@francescobedussi.it>
2016-04-30 16:39:34 +0200

    disable uglify

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -1,9 +1,9 @@
 module.exports = function (grunt) {
 	grunt.registerTask('prod', [
-		'compileAssets',
-		'concat',
-		'uglify',
-		'cssmin',
+		//'compileAssets',
+		//'concat',
+		//'uglify',
+		//'cssmin',
 		'sails-linker:prodJs',
 		'sails-linker:prodStyles',
 		'sails-linker:devTpl',
