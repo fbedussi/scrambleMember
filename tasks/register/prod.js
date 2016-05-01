ae66d90ebec3c86d7cc1e53bf1708f7eb3834d19
fbedussi <francesco@francescobedussi.it>
2016-05-01 15:30:12 +0200

    prod build as local

--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -2,13 +2,18 @@ module.exports = function (grunt) {
 	grunt.registerTask('prod', [
 		//'compileAssets',
 		//'concat',
+    'compileAssets',
+		'linkAssetsBuild',
+		'clean:build',
+		'copy:build'
 		//'uglify',
 		//'cssmin',
-		'sails-linker:prodJs',
-		'sails-linker:prodStyles',
-		'sails-linker:devTpl',
-		'sails-linker:prodJsJade',
-		'sails-linker:prodStylesJade',
-		'sails-linker:devTplJade'
+		//'sails-linker:prodJs',
+		//'sails-linker:prodStyles',
+		//'sails-linker:devTpl',
+		//'sails-linker:prodJsJade',
+		//'sails-linker:prodStylesJade',
+		//'sails-linker:devTplJade'
 	]);
 };
+
